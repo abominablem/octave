09af3d8de714ba8743f17937a88ae21dfa53c104
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2021-12-25 02:41:14 +0000

    Created OctaveMenuBar class based on tk.Menu
    Generates custom events when menu items are clicked

--- base.py
+++ base.py
@@ -7,7 +7,9 @@ Created on Tue Dec 21 22:35:23 2021
 import sys
 sys.path.append("D:\\Users\\Marcus\\Documents\\R Documents\\Coding\\Python\\Packages")
 import tkinter as tk
+import re
 from PIL import Image, ImageTk
+
 from mh_logging import log_class
 import tk_arrange as tka
 from sqlite_tablecon import TableCon
@@ -17,7 +19,7 @@ import futil
 log_class = log_class(g.LOG_LEVEL)
 
 class OctaveFrameBase:
-    """ Base tk.Frame with Octave logo and title bar """
+    """ Base tk.Frame with menu, Octave logo, and title bar """
     @log_class
     def __init__(self, master, title = "Octave"):
         self.master = master
@@ -60,8 +62,74 @@ class OctaveFrameBase:
         self.master.columnconfigure(0, weight = 1)
 
     @log_class
+    def get_widget(self):
+        return self.widget_set
+
+    @log_class
     def grid(self, **kwargs):
         self.widget_set.grid(**kwargs)
+
+    def rowconfigure(self, *args, **kwargs):
+        self.widget_set.rowconfigure(*args, **kwargs)
+
+    def columnconfigure(self, *args, **kwargs):
+        self.widget_set.columnconfigure(*args, **kwargs)
+
+class OctaveMenuBar(tk.Menu):
+    @log_class
+    def __init__(self, master):
+        self._menu_bar_kwargs = {
+            "fg": g.COLOUR_MENU_BAR_FOREGROUND,
+            "activebackground": g.COLOUR_MENU_BAR_ACTIVE_BACKGROUND,
+            "activeforeground": g.COLOUR_MENU_BAR_ACTIVE_FOREGROUND,
+            "tearoff": False,
+            }
+        super().__init__(master, bg = g.COLOUR_MENU_BAR_MAIN_BACKGROUND,
+                       **self._menu_bar_kwargs)
+        self.menus_dict = {}
+        self.add_menu("File", ["New", "Open", "Save", "Save As",
+                               "__seperator__", "Exit", "File"])
+
+        self.add_menu("Tools", ["MIDI Input device", "MIDI Output device",
+                                "Refresh MIDI devices"])
+
+    @log_class
+    def add_menu(self, label, commands):
+        """ Iteratively add a list of labels and commands to a menu"""
+        menu = tk.Menu(
+            self, bg = g.COLOUR_MENU_BAR_BACKGROUND, **self._menu_bar_kwargs)
+        self.menus_dict[label] = menu
+        for command in commands:
+            if command == "__seperator__":
+                menu.add_separator()
+            else:
+                self._add_menu_item(menu, label, command)
+        self.add_cascade(label = label, menu = menu)
+
+    @log_class
+    def _add_menu_item(self, menu, label, command):
+        event_string = self._get_custom_event_string(label, command)
+        callback = lambda: self.event_generate(event_string)
+        menu.add_command(label = command, command = callback)
+        self.bind(event_string, lambda: print(event_string))
+
+    @log_class
+    def _get_custom_event_string(self, label, command):
+        command = command.title().replace(" ", "")
+        command = re.sub("[^\w]", "", command)
+        return "<<%s-%s>>" % (label, command)
+
+    # @log_class
+    # def event_generate(self, sequence, **kw):
+    #     """Generate an event SEQUENCE. Additional
+    #     keyword arguments specify parameter of the event
+    #     (e.g. x, y, rootx, rooty)."""
+    #     print(sequence)
+    #     args = ('event', 'generate', self._w, sequence)
+    #     for k, v in kw.items():
+    #         args = args + ('-%s' % k, str(v))
+    #     self.tk.call(args)
+
 
 class OctaveWindowBase:
     """ Toplevel window with OctaveFrameBase """
@@ -70,8 +138,9 @@ class OctaveWindowBase:
         self.master = master
         self.window = tk.Toplevel(self.master, bg = g.COLOUR_BACKGROUND)
         self.master.eval(f'tk::PlaceWindow {self.window} center')
+        self.menu_bar = OctaveMenuBar(self.window)
+        self.window.config(menu = self.menu_bar)
         self.title_bar = OctaveFrameBase(self.window, title = title)
-        self.title_bar.grid(row = 0, column = 0, **g.GRID_STICKY)
 
     @log_class
     def start(self):
@@ -93,3 +162,35 @@ class OctaveConnections:
 
 octave_db = OctaveConnections(r".\data\octave_sqlite.db",
                               ["books", "records", "tags"])
+
+
+if __name__ == "__main__":
+    class TestApp:
+        @log_class
+        def __init__(self):
+            self.root = tk.Tk()
+            btn_start_rec = tk.Button(
+                self.root, text = "start recording",
+                command = self.start_window,
+                font = ("Constantia", 32, "bold"))
+            btn_start_rec.grid(row = 1, column = 0)
+            self.menu_bar = OctaveMenuBar(self.root)
+            self.root.config(menu = self.menu_bar)
+
+            def test_func(self, *args, **kwargs):
+                print("test func activated")
+
+            self.menu_bar.bind("<<File-Exit>>", test_func)
+
+            self.root.mainloop()
+
+        @log_class
+        def start_window(self):
+            title_bar = OctaveWindowBase(self.root, title = "TEST WINDOW")
+            btn_stop_rec = tk.Button(
+                title_bar.window, text = "stop recording",
+                font = ("Constantia", 32, "bold"))
+            btn_stop_rec.grid(row = 1, column = 0)
+            title_bar.start()
+
+    app = TestApp()
